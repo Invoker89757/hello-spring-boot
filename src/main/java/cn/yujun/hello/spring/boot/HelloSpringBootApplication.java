a874d1313a3c589668ad101ad47eda56230136ee
Invoker89757 <578081401@qq.com>
2019-04-03 20:59:08 +0800

    创建项目

---
+++ src/main/java/cn/yujun/hello/spring/boot/HelloSpringBootApplication.java
@@ -0,0 +1,22 @@
+package cn.yujun.hello.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class HelloSpringBootApplication {
+
+    @RequestMapping(value = "",method = RequestMethod.GET)
+    public String sayHi(){
+        return "Hello Spring Boot123";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringBootApplication.class, args);
+    }
+
+}
